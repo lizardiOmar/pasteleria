8ef861b4b02491b68021965a6267f011aff7c157
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-02 14:45:15 -0500

    Add files via upload

---
+++ MVC/Vistas/EncabezadoLogin.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Pastelería</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1 class=" rounded-pill bg-gradient alert alert-primary border border-primary border-3 text-center fs-1 fw-bolder user-select-none w-50 p-3 mx-auto " >Pastelería</h1>
+	</body>
+</html>
