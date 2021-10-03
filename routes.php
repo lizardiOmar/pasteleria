089725aa274a4a921eac9465f9faf476a5752b11
JonBV <48332894+JonBV@users.noreply.github.com>
2021-10-02 22:36:11 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,3 +2,4 @@
 $route['acceso'] = 'LoginController/vistaAcceso';
 $route['registro'] = 'RegistroController/vistaRegistro';
 $route['registrarProducto'] = 'Registrar_Productos_Controller/vistaRegistrarProducto';
+$route['actualizarProducto'] = 'Actualizar_Productos_Controller/VistaActualizar_Productos';
