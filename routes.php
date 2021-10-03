7a2ff13027a59dfa4ff10478f48f0fa42aedef71
abra95 <91098103+abra95@users.noreply.github.com>
2021-10-02 21:48:07 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,4 +1,5 @@
 <?php
 $route['acceso'] = 'LoginController/vistaAcceso';
 $route['registro'] = 'RegistroController/vistaRegistro';
+$route['registrarProducto'] = 'Registrar_Productos_Controller/vistaRegistrarProducto';
 
