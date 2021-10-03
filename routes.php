bfb1700c1d8b5d8420af2ff8193e85bc9ff01aa7
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-02 22:27:20 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,4 +2,3 @@
 $route['acceso'] = 'LoginController/vistaAcceso';
 $route['registro'] = 'RegistroController/vistaRegistro';
 $route['registrarProducto'] = 'Registrar_Productos_Controller/vistaRegistrarProducto';
-
