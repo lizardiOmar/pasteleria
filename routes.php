9c5fc9f59501aa9c2384d9535591c18aa594a204
kpanes <91098125+kpanes@users.noreply.github.com>
2021-10-03 00:00:20 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -3,3 +3,4 @@ $route['acceso'] = 'LoginController/vistaAcceso';
 $route['registro'] = 'RegistroController/vistaRegistro';
 $route['registrarProducto'] = 'Registrar_Productos_Controller/vistaRegistrarProducto';
 $route['actualizarProducto'] = 'Actualizar_Productos_Controller/VistaActualizar_Productos';
+$route['verCatalogo'] = 'Ver_Catalogo_Controller/Ver_Catalogo';
