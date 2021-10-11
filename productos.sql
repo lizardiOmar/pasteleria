ec9205748fb3f00e21c13a23bdac7b58156a6ddc
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-11 18:37:44 -0500

    Add files via upload

---
+++ productos.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM pasteleria_bd.productos
+LIMIT 0, 1000
+
+-- Date: 2021-10-11 18:36
+*/
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (1,'Tiramisú','El tiramisú nació en Italia. Lo elaboran con un bizcocho de soletas humedecidas en café y ',400,5,2,1);
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (2,'Cheesecake','Originario de Grecia y después introducido a Estados unidos, el cheesecake hoy en día es u',300,3,2,1);
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (3,'Panna cotta','Aunque este postre italiano habitualmente se sirve con frutos rojos, te proponemos una ver',300,5,3,2);
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (4,'Flan de té \'matcha\'','El té \'matcha\' ha irrumpido en nuestros mercados con fuerza. En japonés, ‘matcha’ signific',200,6,3,2);
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (5,'Galletas de mantequilla','Las galletas de mantequilla, también conocidas como galletas danesas, son un éxito interna',10,30,20,3);
+INSERT INTO `` (`ID`,`nombre`,`descripcion`,`precio`,`cantidad`,`cantidad_minima`,`tipo`) VALUES (6,'Galletas de chocolate','Deliciosa y saludable, rústicas y crujientes, son las delicias de los niños y el vicio de ',10,28,20,3);
---
+++ tipos_de_producto.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM pasteleria_bd.tipos_de_producto
+LIMIT 0, 1000
+
+-- Date: 2021-10-11 18:37
+*/
+INSERT INTO `` (`ID`,`Nombre`) VALUES (1,'Pastel');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (2,'Gelatina');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (3,'Galleta');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (4,'Postre');
