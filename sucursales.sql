eddb3d11092569bce8dc29e64e2f4a246ba20bf2
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-11 18:39:47 -0500

    Add files via upload

---
+++ sucursales.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM pasteleria_bd.sucursales
+LIMIT 0, 1000
+
+-- Date: 2021-10-11 18:39
+*/
+INSERT INTO `` (`IdSucursal`,`Nombre`,`Telefono`,`Ciudad`,`Colonia`,`Calle`,`Numero`,`Correo`,`HoraApertura`,`HoraCierre`) VALUES (1,'La paz','55-555-555-22','Xalapa','Progreso','Saltillo','300','lapaz@pasteles.com','8:00','20:00');
