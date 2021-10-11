18790958125c785e859ea7cb5dbd96e440b59229
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-11 18:38:36 -0500

    Delete routes.php

--- routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$route['acceso'] = 'LoginController/vistaAcceso';
-$route['registro'] = 'RegistroController/vistaRegistro';
-$route['registrarProducto'] = 'Registrar_Productos_Controller/vistaRegistrarProducto';
-$route['actualizarProducto'] = 'Actualizar_Productos_Controller/VistaActualizar_Productos';
-$route['verCatalogo'] = 'Ver_Catalogo_Controller/Ver_Catalogo';
