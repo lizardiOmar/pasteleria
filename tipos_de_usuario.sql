edbff9977d1f711cb5bdee9f7f4f2a2df9277eea
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-11 18:42:21 -0500

    Add files via upload

---
+++ tipos_de_usuario.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select * from tipos_de_usuario
+LIMIT 0, 1000
+
+-- Date: 2021-10-11 18:42
+*/
+INSERT INTO `` (`ID`,`Nombre`) VALUES (1,'administrador');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (2,'encargado_de_elaboracion_de_productos');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (3,'repartidor');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (4,'encargado_de_almacen');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (5,'cajero');
+INSERT INTO `` (`ID`,`Nombre`) VALUES (6,'cliente');
