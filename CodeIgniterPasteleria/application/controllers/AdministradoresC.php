232c77749966cff6d51980ff34bf5332bd3dbac7
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-10-13 16:10:45 -0500

    Add files via upload

--- CodeIgniterPasteleria/application/controllers/AdministradoresC.php
+++ CodeIgniterPasteleria/application/controllers/AdministradoresC.php
@@ -59,25 +59,130 @@ class AdministradoresC extends CI_Controller {
 			show_404();
 			//echo 'Cáspita ... no tenemos una pagina para esto!';
 		}
+		$datos['msg_1'] = "";
 		$administrador=$this->usuariosM->getUsuarioById($idAdministrador);
-		if($administrador === false){
-			//Cargar vista de redireccion al login
+		$producto=$this->productosM->getProductoById($idProducto);
+		$datos['administrador']=$administrador;
+		$datos['producto']=$producto;
+		$this->form_validation->set_rules(
+			'nombre',//nombre del imput del formulario
+			'nombre',//etiqueta para mensajes de salida
+			'min_length[5]|max_length[30]|trim',//minimo 5 caracteres, maximo 30, correo electrónico único, correo electronico con formato válido 
+			array(
+				'min_length' => 'El minímo de caracteres para modificar el %s son 2.',
+				'max_length' => 'El máximo de caracteres para modificar el %s son 30.',
+				'is_unique' => 'El %s no puede ser igual a alguno ya almacenado.'
+			)
+		);
+		$this->form_validation->set_rules(
+			'descripcion',//nombre del imput del formulario
+			'descripción',//etiqueta para mensajes de salida
+			'min_length[20]|max_length[90]|alpha_numeric_spaces|trim|is_unique[productos.nombre]',//minimo 5 caracteres, maximo 30, correo electrónico único, correo electronico con formato válido 
+			array(
+				'min_length' => 'El minímo de caracteres para modificar la %s son 2.',
+				'max_length' => 'El máximo de caracteres para modificar la %s son 90.',
+				'alpha_numeric' => 'Solo se permiten numeros y letras para la %s',
+				'is_unique' => 'La %s no puede ser igual a alguno ya almacenado.'
+			)
+		);
+		$this->form_validation->set_rules(
+			'precio',//nombre del imput del formulario
+			'precio',//etiqueta para mensajes de salida
+			'min_length[2]|max_length[3]|numeric|trim',//minimo 5 caracteres, maximo 30, correo electrónico único, correo electronico con formato válido 
+			array(
+				'min_length' => 'El minímo de caracteres para modificar el %s son 2.',
+				'max_length' => 'El máximo de caracteres para modificar el %s son 3.',
+				'numeric' => 'Solo se permiten numeros y letras para el %s'
+			)
+		);
+		$this->form_validation->set_rules(
+			'cantidad',//nombre del imput del formulario
+			'cantidad',//etiqueta para mensajes de salida
+			'min_length[1]|max_length[3]|numeric|trim',//minimo 5 caracteres, maximo 30, correo electrónico único, correo electronico con formato válido 
+			array(
+				'min_length' => 'El minímo de caracteres para modificar la %s son 2.',
+				'max_length' => 'El máximo de caracteres para modificar la %s son 3.',
+				'numeric' => 'Solo se permiten numeros y letras para la %s'
+			)
+		);
+		$this->form_validation->set_rules(
+			'cantidad_minima',//nombre del imput del formulario
+			'cantidad minima',//etiqueta para mensajes de salida
+			'min_length[1]|max_length[3]|numeric|trim',//minimo 5 caracteres, maximo 30, correo electrónico único, correo electronico con formato válido 
+			array(
+				'min_length' => 'El minímo de caracteres para modificar la %s son 2.',
+				'max_length' => 'El máximo de caracteres para modificar la %s son 3.',
+				'numeric' => 'Solo se permiten numeros y letras para la %s'
+			)
+		);
+		
+		if ($this->form_validation->run() === FALSE){
+			$this->load->view('administradores/editarProductoForm', $datos);
 		}else{
-			if($administrador['tipo'] === '1'){
-				//Importar modelo de usuarios
-				$producto=$this->productosM->getProductoById($idProducto);
-				if($producto === FALSE){
-					show_404();
-				}else{
-					//echo json_encode($producto).'<br>';
-					//echo json_encode($administrador);
-					$datos['producto']=$producto;
-					$datos['administrador']=$administrador;
-					$this->load->view('administradores/editarProductoForm', $datos);
-				}
-				
+			$producto_new['id']=$idProducto;
+			$producto_new['nombre']=$this->input->post('nombre');
+			$producto_new['descripcion']=$this->input->post('descripcion');
+			$producto_new['precio']=$this->input->post('precio');
+			$producto_new['cantidad']=$this->input->post('cantidad');
+			$producto_new['cantidad_minima']=$this->input->post('cantidad_minima');
+			$producto_new['tipo']=$this->input->post('tipo');
+			$formValido=false;
+			
+			if($producto_new['nombre'] == $producto['nombre']){
+				$datos['msg_1'] = "No puedes escribir el mismo nombre que ya existe en la base de datos.";
+				//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+				//$this->load->view('administradores/editarProductoForm', $datos);
 			}else{
-				echo "Vaya ".$administrador['nombres'].$administrador['apellidos'].", parece que no eres administrador, así que no puedes editar esto.";
+				if($producto_new['nombre'] != null){
+					$formValido=true;
+				}
+				if($producto_new['descripcion'] == $producto['descripcion']){
+					$datos['msg_1'] = "No puedes escribir la misma descripcion que ya existe en la base de datos.";
+					//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+					//$this->load->view('administradores/editarProductoForm', $datos);
+				}else{
+					if($producto_new['descripcion'] != null){
+						$formValido=true;
+					}
+					if($producto_new['precio'] == $producto['precio']){
+						$datos['msg_1'] = "No puedes escribir el mismo precio que ya existe en la base de datos.";
+						//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+						//$this->load->view('administradores/editarProductoForm', $datos);
+					}else{
+						if($producto_new['precio'] != null){
+							$formValido=true;
+						}
+						if($producto_new['cantidad'] == $producto['cantidad']){
+							$datos['msg_1'] = "No puedes escribirla misma cantidad que ya existe en la base de datos.";
+							//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+							//$this->load->view('administradores/editarProductoForm', $datos);
+						}else{
+							if($producto_new['cantidad'] != null){
+								$formValido=true;
+							}
+							if($producto_new['cantidad_minima'] == $producto['cantidad_minima']){
+								$datos['msg_1'] = "No puedes escribirla misma cantidad miníma que ya existe en la base de datos.";
+								//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+								//$this->load->view('administradores/editarProductoForm', $datos);
+							}else{
+								if($producto_new['cantidad_minima'] != null){
+									$formValido=true;
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			if($formValido === false){
+				//$datos['msg_1'] = "No hay ningún cambio detectado.";
+				//$producto_new['nombre'][strlen($producto_new['nombre'])-1]='.';
+				$this->load->view('administradores/editarProductoForm', $datos);
+			}else{
+				$datos['msg_1'] = "";
+				//$producto_new['nombre'][strlen($producto_new['nombre'])]='.';
+				$datos['producto_new']=$producto_new;
+				$this->load->view('administradores/resultadoEditarProducto', $datos);
 			}
 		}
 	}
