57695e69c37e2e21b0f6d0e0dbab556b94f2a233
abra <91098103+abra95@users.noreply.github.com>
2021-10-27 17:10:37 -0500

    Update ClientesC.php

--- CodeIgniterPasteleria/application/controllers/ClientesC.php
+++ CodeIgniterPasteleria/application/controllers/ClientesC.php
@@ -44,4 +44,32 @@ class ClientesC extends CI_Controller {
 			$this->load->view('clientes/catalogo', $datos);
 		}
 	}
+	
+	public function busquedaTipo($id, $tipoProducto){
+		if (!file_exists(APPPATH.'views/clientes/cabecerabusqueda.php')){
+			show_404();
+			//echo 'Cáspita ... no tenemos una pagina para esto!';
+		}
+		$cliente=$this->usuariosM->getUsuarioById($id);
+		if($cliente === false){
+			//Cargar vista de redireccion al login
+		}else{
+			//cargar la plantilla de catalogo del sitio de los clientes
+			$datos['cliente']=$cliente;
+			$this->load->model('tipoProductosM');
+			$datos['tipoProductos'] = $this->tipoProductosM->getTipoProductos();
+			$this->load->model('productosM');
+			$datos['productos'] = $this->productosM->getProductoByTipo($tipoProducto);
+			$this->load->view('clientes/cabecera', $datos);
+			$this->load->view('clientes/cabecerabusqueda', $datos);
+			if($datos['productos']===false){
+				$this->load->view('clientes/sinProducto', $datos);
+			}else{
+				$this->load->view('clientes/catalogo', $datos);
+			}
+			
+		}
+
+	}
+	
 }
