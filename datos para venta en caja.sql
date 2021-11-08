2dc6e7c31f59df96c7f2345ca31132e3925aa5ce
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-11-08 17:19:12 -0600

    Add files via upload

---
+++ datos para venta en caja.sql
@@ -0,0 +1,6 @@
+INSERT INTO `pasteleria_bd`.`tipos_de_pedido` (`ID`, `Nombre`) VALUES (1, 'Caja');
+
+INSERT INTO `pasteleria_bd`.`estados_de_pedido` (`ID`, `Estado`) VALUES (0, 'CAJA en captura');
+INSERT INTO `pasteleria_bd`.`estados_de_pedido` (`ID`, `Estado`) VALUES (0, 'CAJA vendido');
+INSERT INTO `pasteleria_bd`.`estados_de_pedido` (`ID`, `Estado`) VALUES (0, 'CAJA cancelado');
+
