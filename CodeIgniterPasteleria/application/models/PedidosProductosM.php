b60b43a902b7883048fd4556cf14d9db3e2c7b86
abra <91098103+abra95@users.noreply.github.com>
2021-11-16 18:00:36 -0600

    Update PedidosProductosM.php

--- CodeIgniterPasteleria/application/models/PedidosProductosM.php
+++ CodeIgniterPasteleria/application/models/PedidosProductosM.php
@@ -15,13 +15,15 @@ class PedidosProductosM extends CI_Model {
 		$this->db->insert('pedidos_productos', $datos);
 		return $this->db->insert_id();
 	}
-	public function getPedidoProductoById($id){
-	$consulta = $this->db->query("SELECT * FROM pedidos_productos WHERE id='{$id}'");
-		$pedidos_productos = $consulta->row_array();
-		if($pedidos_productos === null){
-			return false;
-		}else{
-			return $pedidos_productos;
-		}
+	public function getProductosbyPedido($idPedido){
+		$this->db->select('*');
+		    $this->db->from('pedidos_productos');
+    		$this->db->where('ID_Pedido', $idPedido );
+		    $query = $this->db->get();
+		    if($query->result_array() == null){
+		    	return false;
+		    }else{
+		    	return $query->result_array(); 
+		    }
 	}
 }
