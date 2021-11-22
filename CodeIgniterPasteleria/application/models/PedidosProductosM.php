ddd3d7e55bb026604b02a441c8c40a72a05948c0
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-11-22 15:57:53 -0600

    Add files via upload

--- CodeIgniterPasteleria/application/models/PedidosProductosM.php
+++ CodeIgniterPasteleria/application/models/PedidosProductosM.php
@@ -15,11 +15,20 @@ class PedidosProductosM extends CI_Model {
 		$this->db->insert('pedidos_productos', $datos);
 		return $this->db->insert_id();
 	}
+	public function getPedidoProductoById($id){
+	$consulta = $this->db->query("SELECT * FROM pedidos_productos WHERE id='{$id}'");
+		$pedidos_productos = $consulta->row_array();
+		if($pedidos_productos === null){
+			return false;
+		}else{
+			return $pedidos_productos;
+		}
+	}
 	public function getProductosbyPedido($idPedido){
 		$this->db->select('*');
-		    $this->db->from('pedidos_productos');
-    		$this->db->where('ID_Pedido', $idPedido );
-		    $query = $this->db->get();
+		$this->db->from('vw_productosVentas');
+    	$this->db->where('idPedido', $idPedido );
+		$query = $this->db->get();
 		    if($query->result_array() == null){
 		    	return false;
 		    }else{
